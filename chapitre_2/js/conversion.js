2d66395ac71d4ada0de8e36f3c3342092743c444
darksh3ll <energie.sud83@gmail.com>
2018-05-22 00:08:34 +0200

    finit exercice tva et conversion

--- chapitre_2/js/conversion.js
+++ chapitre_2/js/conversion.js
@@ -1,3 +1,6 @@
 /*
 Exercice : convertir des degrés celsius en fahrenheit
 */
+const saisieTempCelsius = Number(prompt("saisir une temperature"));
+const ConversionCelsiusFahrenheit = (saisieTempCelsius * 9/5) + 32;
+alert(`${ConversionCelsiusFahrenheit} ° Fahrenheit`);
