3dc5cb584551f47c87cbdb242e8f806d46e68301
darksh3ll <energie.sud83@gmail.com>
2018-05-21 18:18:23 +0200

    niveau1

--- chapitre_2/js/tva.js
+++ chapitre_2/js/tva.js
@@ -3,3 +3,8 @@ Exercice : calculer un prix TTC à partir d'un prix HT
 */
 
 // Saisie du prix hors taxes
+
+const prixHt = Number(prompt("Entrez le prix HT"));
+const prixTtc = prixHt*1.2;
+const montantTva = Math.round(prixTtc - prixHt);
+alert(`le prix TTC est de ${prixTtc} Euros avec une tva de ${montantTva} Euros`);
