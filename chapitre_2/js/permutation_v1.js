fbef7cbae122398f94f7462c88dcdb92e0338369
darksh3ll <energie.sud83@gmail.com>
2018-05-22 00:22:20 +0200

    debut exercice permutation

--- chapitre_2/js/permutation_v1.js
+++ chapitre_2/js/permutation_v1.js
@@ -1,4 +1,12 @@
 /* 
 Exercice : inverser les valeurs des variables nombre1 et nombre2.
 */
+var nombre1 = 5;
+var nombre2 = 3;
+var nombre3 = nombre1; //vaut 5
+nombre2 = nombre3; //vaut 5
+nombre1 = nombre2; //
 
+
+console.log(nombre1); // Doit afficher 3
+console.log(nombre2); // Doit afficher 5
