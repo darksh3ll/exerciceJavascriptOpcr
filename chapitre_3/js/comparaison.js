43d1684bf2e65c62929634a70405d9c7d0097b9c
darksh3ll <energie.sud83@gmail.com>
2018-05-22 21:04:33 +0200

    avancement du projet

--- chapitre_3/js/comparaison.js
+++ chapitre_3/js/comparaison.js
@@ -2,3 +2,21 @@
 Exercice : comparer deux nombres
 */
 
+var nbr1 = Number(prompt("Entrez un nombres entres 0 et 10"));
+if ((nbr1 >= 0) && (nbr1 < 10)) {
+    var nbr2 = Number(prompt("Entrez un deuxieme nombres entres 0 et 10"));
+}  if ((nbr2 >= 0) && (nbr2 < 10)) {
+        var resultat = nbr1 + nbr2;
+        alert(`${nbr1} + ${nbr2} = ${resultat} `)
+    }else {
+        alert ("trop grand");
+    }
+
+
+
+    // Algo
+    // l'utilsateur rentre un 1 er nombres
+    // si le nombre est compris entre 0 et 10
+    // l'utisateur rentre un deuxieme nombres
+    // si le deuxieme nombres est compris entre 0 et 10
+    // alors on n'affiche le calcul a l'ecran
--- chapitre_3/js/jour_suivant_v1.js
+++ chapitre_3/js/jour_suivant_v1.js
@@ -1,4 +1,30 @@
 /* 
 Exercice : afficher le jour suivant
 */
+var joursSuivant =  prompt("Quelles jours sommes nous");
+switch (joursSuivant) {
+    case "lundi":
+        alert("Demain mardi")
+        break;
+    case "mardi":
+        alert("Demain Mercredi")
+        break;
+    case "mercredi":
+        alert("Demain jeudi")
+        break;
+    case "jeudi":
+        alert("Demain vendredi")
+        break;
+    case "vendredi":
+        alert("Demain samedi")
+        break;
+    case "samedi":
+        alert("Demain dimanche")
+        break;
+    case "dimanche":
+        alert("Demain Lundi")
+        break;
+    default:
+    alert("J'ai dit quelles jours sommes nous je le repeterais pas 2 fois")
 
+}
