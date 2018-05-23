fb3cbcf330320e7ab49685c0afed65c11c47996b
darksh3ll <energie.sud83@gmail.com>
2018-05-23 19:04:11 +0200

    bac.js exercice termine

--- chapitre_3/js/bac.js
+++ chapitre_3/js/bac.js
@@ -1,4 +1,11 @@
 /* 
 Exercice : trouver le résultat au bac selon la moyenne
 */
-
+const moyenneCandidat = Number(prompt("Entrez la moyenne du candidat"));
+if (moyenneCandidat < 10) {
+    alert("Candidat recalé")
+}else if ((moyenneCandidat >= 10) && (moyenneCandidat <= 12)) {
+    alert("Candidat recus mais doit faire des efforts")
+}else {
+    alert("Candidat recus avec appreciation trés bon eleves")
+}
