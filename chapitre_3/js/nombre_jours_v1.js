109b33b3066857c010d7001848dab202902cb346
darksh3ll <energie.sud83@gmail.com>
2018-05-24 00:16:11 +0200

    exercice nombres de jours finit

--- chapitre_3/js/nombre_jours_v1.js
+++ chapitre_3/js/nombre_jours_v1.js
@@ -2,4 +2,27 @@
 Exercice : afficher le nombre de jours d'un mois
 */
 
-v
+const numeroMois = Number(prompt("Entrez le numero d'un mois au hasard"));
+
+switch (numeroMois) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        alert("mois a 31 jours")
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        alert("mois a 30 jours")
+        break;
+    case 2:
+        alert("mois a 28 jours")
+        break;
+    default:
+        alert("Mois non reconnues")
+}
