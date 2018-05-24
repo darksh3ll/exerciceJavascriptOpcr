16b1f4cd94e172e31d7b31486c367b2ae2401cc3
darksh3ll <energie.sud83@gmail.com>
2018-05-24 13:44:53 +0200

    démarrage exercices heures suivantes

--- chapitre_3/js/heure_suivante.js
+++ chapitre_3/js/heure_suivante.js
@@ -2,3 +2,17 @@
 Exercice : afficher l'heure une seconde plus tard
 */
 
+let heures   = 12;
+let minutes  = 59;
+let secondes = 59;
+
+if ((heures >= 0) && (heures <= 23) && (minutes >=0) && (minutes <=59) && (secondes >=0) && (secondes <=59)) {
+    secondes++;
+    if (secondes === 59) {
+        secondes=0;
+        minutes++;
+        if (minutes===60) {
+            
+        }
+    }
+}
