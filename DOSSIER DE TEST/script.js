abc8b893df7230ed84d678310b152965bb0f615c
darksh3ll <energie.sud83@gmail.com>
2018-05-26 00:06:06 +0200

    test javascript

--- DOSSIER DE TEST/script.js
+++ DOSSIER DE TEST/script.js
@@ -1,11 +1,10 @@
-const tableau = [60,60,23,80];
+const tableau = [5,8];
 let result = [];
+let parametres = 0;
 for ( let i = 0; i < tableau.length; i++ ) {
-    result.push(tableau[i]);
-    if (result[0] < tableau[i]) {
-        result.pop();
+    if (tableau[i] >= parametres ) {
+        result.push(tableau[i]);
     }
-
 }
 console.log(result);
 
