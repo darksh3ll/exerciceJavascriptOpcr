60139c7b34f8c148b9182f5082d5425cc1c5d93c
darksh3ll <energie.sud83@gmail.com>
2018-05-25 20:33:33 +0200

    exercice plus grand

--- DOSSIER DE TEST/script.js
+++ DOSSIER DE TEST/script.js
@@ -1,2 +1,12 @@
+const tableau = [60,60,23,80];
+let result = [];
+for ( let i = 0; i < tableau.length; i++ ) {
+    result.push(tableau[i]);
+    if (result[0] < tableau[i]) {
+        result.pop();
+    }
+
+}
+console.log(result);
 
 
