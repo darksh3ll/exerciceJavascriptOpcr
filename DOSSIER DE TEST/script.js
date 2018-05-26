85020f1a6b010929062d28ce5d2b3b4d3ce1d1b1
darksh3ll <energie.sud83@gmail.com>
2018-05-26 12:58:08 +0200

    avancement

--- DOSSIER DE TEST/script.js
+++ DOSSIER DE TEST/script.js
@@ -1,5 +1,9 @@
-let tours = 12 ;
-while (tours >= 0) {
-    console.log(tours);
-    tours--;
-}
+//Variables de base de calcul
+const distance = 20000; //Entrez la distance en km de votre destination
+const vitesse = 300000; //Entrez votre vitesse de croisiere
+
+//Calcul fondamental
+const tempsParcours = distance/vitesse*60;//temps parcours  minutes
+const allerRetour = 60/tempsParcours
+console.log(`durer ${tempsParcours} seconde et ${allerRetour} Aller Retour en 1 secondes`)
+
