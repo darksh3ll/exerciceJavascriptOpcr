d05b6b27dcd9b9c867e5fa66a35b6377484bf5a3
darksh3ll <energie.sud83@gmail.com>
2018-05-26 09:29:32 +0200

    création d`un dossiers qui compare es5 a es6 sur la bouche forof

---
+++ NOTE JAVSCRIPT/forOf.js
@@ -0,0 +1,12 @@
+console.log("ES5");
+//ES5
+const arr = [12,23,54,65];
+for ( let i = 0; i < arr.length; i++ ) {
+    console.log(arr[i]);
+}
+console.log("ES6");
+//ES6
+const arr1 = [12,23,54,65,100];
+for (let nombres of arr1 ) {
+    console.log(nombres);
+} 
