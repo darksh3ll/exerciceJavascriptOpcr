f380eddefbff360c04b95040c5c183ec8d9f5d0f
darksh3ll <energie.sud83@gmail.com>
2018-05-27 11:30:20 +0200

    avancement des exercices

--- chapitre_4/js/parite.js
+++ chapitre_4/js/parite.js
@@ -1,4 +1,15 @@
 /* 
 Exercice : afficher les nombres pairs et impairs
 */
-console.log(40%);
+const saisie = 20;
+if ((saisie >= 1) && (saisie <= 20)) {
+    for ( let i = 1; i <= saisie; i++ ) {
+        if (i % 2 === 0) {
+            console.log(`${i} pair`);
+        }else {
+            console.log(`${i} impair`);
+        }
+    }
+}else {
+    console.log("Attention entres 1 et 10 RECOMMENCE!!!!")
+}
--- chapitre_4/js/saisie.js
+++ chapitre_4/js/saisie.js
@@ -1,4 +1,8 @@
 /* 
 Exercice : contrôle de saisie
 */
-
+let num = Number(prompt("Entrez un chiffres "));
+while (num < 100) {
+        num = Number(prompt( num + " "  + "est inferieur a 100 Recommence"));
+}
+alert("bravo ton dernier chiffres etait" + " " + num);
