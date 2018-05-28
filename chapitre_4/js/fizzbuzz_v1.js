1330b8f1026159a638e5ea8cc9084775f9e943ad
darksh3ll <energie.sud83@gmail.com>
2018-05-28 21:04:10 +0200

    exercice chapitre 4 finis

--- chapitre_4/js/fizzbuzz_v1.js
+++ chapitre_4/js/fizzbuzz_v1.js
@@ -3,3 +3,12 @@ Exercice : FizzBuzz
 */
 
 // Solution utilisant une série de if/else
+for (let i = 0; i < 100; i++) {
+    if (i%3===0) {
+        console.log("FIZZ")
+    }else if (i%5===0){
+        console.log("Buzz");
+    }else {
+        console.log(i);
+    }
+}
--- chapitre_4/js/fizzbuzz_v2.js
+++ chapitre_4/js/fizzbuzz_v2.js
@@ -3,3 +3,14 @@ Exercice : FizzBuzz
 */
 
 // Solution construisant le message à afficher par concaténation
+for (let i = 1; i < 200; i++) {
+    if ((i%3===0)&&(i%5===0)) {
+        console.log(`${i} divisible par 3 et 5`)
+    }else if (i%3===0){
+        console.log(`${i} divisible par 3`)
+    }else if (i%5===0){
+console.log(`${i} divisible par 5`)
+    }else {
+        console.log(i);
+    }
+}
