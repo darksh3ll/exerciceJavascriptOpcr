16f416dbee5c6fd25b2752e0fe6356b0a73a40a2
darksh3ll <energie.sud83@gmail.com>
2018-05-29 18:19:21 +0200

    2 eme avancement

--- chapitre_5/js/bonjour.js
+++ chapitre_5/js/bonjour.js
@@ -2,4 +2,19 @@
 Exercice : fonction bonjour avec saisies utilisateur
 */
 
-// Renvoie un message de bienvenue
+//Renvoie un message de bienvenue
+
+
+let temp = 0;
+
+function tempsHeures(vitesse,km) {
+    console.log(km/vitesse);
+}
+function tempsMinutes(vitesse,km) {
+    const vitesse = 30;
+    const km = 1000;
+    temp=(km/vitesse)*3600;
+}
+
+tempsMinutes();
+console.log(temp);
