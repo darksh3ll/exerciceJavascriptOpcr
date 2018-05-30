36e95db9b4d69ece08a749604b70662746d66c9e
darksh3ll <energie.sud83@gmail.com>
2018-05-30 10:32:03 +0200

    niveau2

---
+++ DOSSIER DE TEST/script.js
@@ -0,0 +1,9 @@
+function adition(nbr1,operateur,nbr2) {
+    return nbr1
+}
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(carre(i))
+    
+}
--- chapitre_5/js/carre.js
+++ chapitre_5/js/carre.js
@@ -3,3 +3,13 @@ Exercice : fonction carré d'un nombre
 */
 
 // Renvoie le carré d'un nombre
+function carre(x) {
+    console.log(`Tu ma demander d'afficher ${x} nombres Voici le resulat `)
+    for (let i = 0; i <= x; i++) {
+
+        console.log(`carre de ${i} = ${i*i}`);
+    }
+
+}
+
+carre(25);
