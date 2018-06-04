133a3cc1ab08b30f95b3c07897a80c409d48ea9b
darksh3ll <energie.sud83@gmail.com>
2018-06-04 17:44:12 +0200

    exercice périmètre d'un cercle

--- chapitre_5/js/cercle.js
+++ chapitre_5/js/cercle.js
@@ -3,3 +3,12 @@ Exercice : périmètre et aire d'un cercle
 */
 
 // Renvoie le périmètre d'un cercle
+function perimetreCircle(x) {
+    return (x*2) * Math.PI;
+}
+
+function aireCircle(x) {
+    return Math.PI * Math.pow(x,2);
+}
+const number = 5 ;
+console.log(`le périmetre de ${number} est de ${perimetreCircle(number).toFixed(2)} avec une aire de ${aireCircle(number).toFixed(2)} Cm2 `);
