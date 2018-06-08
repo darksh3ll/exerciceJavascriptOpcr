e4c4d5c919c89f0f01865e8cc95e05c980189e19
darksh3ll <energie.sud83@gmail.com>
2018-06-08 18:13:20 +0200

    eeee

--- DOSSIER DE TEST/script.js
+++ DOSSIER DE TEST/script.js
@@ -1,9 +1,15 @@
-const arr = [0,1,1];
-const fibo = [];
-const longueurTableau = arr.length;
-const num1 = arr[longueurTableau-1];
-const num2 = arr[longueurTableau-2];
 
+const ordinateur    = {
+    marque          : "apple",
+    type            : "latop",
+    ref             : "Mac Book Pro",
+    annee           : 2017,
+    prix            : 3000
+};
 
-    
-console.log(fibo);
+ordinateur.marque = "asus";
+ordinateur.marque = "hp";
+
+console.log(`ordinateur de marque ${ordinateur.marque} de type ${ordinateur.type} référence ${ordinateur.ref} annee ${ordinateur.annee}`);
+
+console.log(ordinateur.prix);
